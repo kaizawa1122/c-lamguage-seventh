a1a9f416add016700b0a82772fb8ea62fbef91e4
kaizawa1122 <kai109870@gmail.com>
2020-06-22 11:14:58 +0900

    add files 7-4.c

---
+++ 7-4.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void minscanf(char *fmt, ...);
+
+int main(void)
+{
+	int day,year;
+	char monthname[20];
+
+	minscanf("%d %s %d \n",&day,monthname, &year);
+	printf("day = %d monthname = %s year = %d \n",day,monthname,year);
+
+	return 0;
+}
+
+void minscanf(char *fmt, ...)
+{
+	va_list ap;
+	char *p,*sval;
+	int *ival;
+	float *fval;
+
+	va_start(ap,fmt);
+	for(p = fmt; *p; p++)
+	{
+		if (*p != '%')
+		{
+			continue;
+		}
+
+		switch(*++p)
+		{
+			case 'd':
+				ival = va_arg(ap,int *);
+				scanf("%d",ival);
+				break;
+			case 'f':
+				fval = va_arg(ap,float *);
+				scanf("%f",fval);
+				break;
+			case 's':
+				sval = va_arg(ap,char *);
+				scanf("%s",sval);
+				break;
+			default:
+				break;
+		}
+	}
+
+	va_end(ap);
+}
+
+
